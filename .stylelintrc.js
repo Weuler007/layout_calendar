08ab82c8ddd1ff988a48d0c039cf8ee06213b7ec
Weuler de Oliveira Vieira <jhdlfdf@hotmail.com>
2026-06-03 23:40:50 -0300

    added  puppetter 22

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,5 +3,11 @@ module.exports = {
   plugins: [
     "stylelint-scss"
   ],
-  rules: {}
+  rules: {
+    // Impede quebra de linha após operadores como *, +, -, /
+    "scss/operator-no-newline-after": true,
+
+    // (opcional) também pode reforçar que não haja quebra antes do operador
+    "scss/operator-no-unspaced": true
+  }
 };
