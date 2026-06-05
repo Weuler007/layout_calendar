008a9c57fff2e4dbb2cdf1dcaddc9073760293c1
Weuler de Oliveira Vieira <jhdlfdf@hotmail.com>
2026-06-05 11:58:56 -0300

    att files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,11 +3,5 @@ module.exports = {
   plugins: [
     "stylelint-scss"
   ],
-  rules: {
-    // Impede quebra de linha após operadores como *, +, -, /
-    "scss/operator-no-newline-after": true,
-
-    // (opcional) também pode reforçar que não haja quebra antes do operador
-    "scss/operator-no-unspaced": true
-  }
+  rules: {}
 };
